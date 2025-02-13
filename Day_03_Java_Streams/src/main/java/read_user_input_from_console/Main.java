ccf1c5cfd5fe710174da5710cae4a6cbd920f592
R-scan1 <deepr7590@gmail.com>
2025-02-13 16:31:51 +0530

    [Raj] Add. read_user_input_from_console problem

---
+++ Day_03_Java_Streams/src/main/java/read_user_input_from_console/Main.java
@@ -0,0 +1,8 @@
+package read_user_input_from_console;
+
+public class Main {
+    public static void main(String[] args) {
+        String destinationPath="src/main/resources/UserDetailsProblem_03.txt";
+        UserInputToFile.createFile(destinationPath);
+    }
+}
