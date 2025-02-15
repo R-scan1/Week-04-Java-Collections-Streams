05ce6c7c1ef2828893b98635a1f9b49b8e344205
R-scan1 <deepr7590@gmail.com>
2025-02-15 21:56:25 +0530

    [Raj] Add. testing_parameterized_tests problem

---
+++ Day_05_Regex_and_JUnit/src/main/java/junit/testing_parameterized_tests/NumberUtils.java
@@ -0,0 +1,10 @@
+package junit.testing_parameterized_tests;
+
+
+public class NumberUtils {
+
+    // Checks if a number is even
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
